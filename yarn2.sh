c3fa763779e4ee75882c5aabce8148e25b5cef5e
Guido D'Orsi <guido.dorsi@immobiliare.it>
2021-07-26 17:15:21 +0200

    fix: corrects yarn2 project setup

--- yarn2.sh
+++ yarn2.sh
@@ -3,5 +3,5 @@
 yarn add husky@latest --dev \
   && yarn add is-ci \
   && yarn dlx husky-init --yarn2 \
-  && npm exec -- github:gdorsi/husky-4-to-7 --remove-v4-config
+  && yarn dlx @dorsi/husky-4-to-7 --remove-v4-config
 
